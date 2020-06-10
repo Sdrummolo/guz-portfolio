c3c8993832c445d976bdf13439ad3cad176d1ca2
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-06-10 09:48:32 +0100

    fixed mobile height

--- src/components/utilities/wrapper.js
+++ src/components/utilities/wrapper.js
@@ -3,9 +3,9 @@ import styled from "styled-components"
 // Disallows overflow-x
 // Position relative is required in order to work
 const Wrapper = styled.div`
-  /* position: relative; */
-  /* min-height: 100vh; */
-  min-height: -webkit-fill-available;
-  overflow-x: hidden;
+  position: relative;
+  min-height: 100vh;
+  /* min-height: -webkit-fill-available; */
+  overflow: hidden;
 `
 export default Wrapper
